7e0954caedc3cfc5e79c3e14fe8312da7201857a
weixianwei <weixianwei4@126.com>
2017-04-17 11:29:10 +0800

    test composer

---
+++ src/Repository/rest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yearnwilling
+ * Date: 2017/4/17
+ * Time: 上午11:22
+ */
+
+namespace Repository;
+
+
+class rest
+{
+    public static function info()
+    {
+        echo "This is rest!";
+    }
+
+
+}
