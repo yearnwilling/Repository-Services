421b1f4e3105177de67334767cb719aad3ad4bed
weixianwei <weixianwei4@126.com>
2017-04-18 23:33:00 +0800

    fix

---
+++ src/Rsc/Repository/BaseRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yearnwilling
+ * Date: 2017/4/18
+ * Time: 下午11:28
+ */
+
+namespace Repository_services\Rsc\Repository;
+
+
+class BaseRepository
+{
+    public static function rest() {
+        echo 'this is br';
+    }
+}
